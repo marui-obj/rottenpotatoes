5faee99e02d9c13313f0f6995920c2169698e21c
echo-centos <s6201012610044@email.kmutnb.ac.th>
2021-08-22 17:13:06 +0700

    init

---
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -0,0 +1,40 @@
+class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+    
+    def show
+        id = params[:id]
+        @movie = Movie.find(id)
+    end
+
+    def new
+        @movie = Movie.new
+        # default: render 'new' template
+    end
+
+    def create
+        @movie = Movie.create!(params[:movie].permit(:title,:rating,:release_date))
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
+    end
+
+    def edit
+        @movie = Movie.find params[:id]
+    end
+
+    def update
+        @movie = Movie.find params[:id]
+        permitted = params[:movie].permit(:title,:rating,:release_date)
+        @movie.update(permitted)
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)
+    end
+    
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        flash[:notice] = "Movie '#{@movie.title}' deleted."
+        redirect_to movies_path
+    end
+end
