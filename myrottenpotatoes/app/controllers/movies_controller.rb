2cd8f6d1ca3201ab9744e915c6270c2ef669a8a8
echo-centos <s6201012610044@email.kmutnb.ac.th>
2021-08-23 08:35:32 +0700

    Project_4

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -1,22 +1,29 @@
 class MoviesController < ApplicationController
+    before_action :check_for_cancel, :only => [:create, :update]
     def index
-        @movies = Movie.all
+        @movies = Movie.all.sort_by{ |name| name.title}
     end
     
     def show
         id = params[:id]
-        @movie = Movie.find(id)
+        begin
+            @movie = Movie.find(id)
+
+        rescue
+            flash[:notice] = "No movie with the given ID could be found."
+            redirect_to movies_path
+        end
+
     end
 
     def new
         @movie = Movie.new
-        # default: render 'new' template
     end
 
     def create
-        @movie = Movie.create!(params[:movie].permit(:title,:rating,:release_date))
+        @movie = Movie.create!(params[:movie].permit(:title,:description,:rating,:release_date))
         flash[:notice] = "#{@movie.title} was successfully created."
-        redirect_to movies_path
+        redirect_to movie_path(@movie)
     end
 
     def edit
@@ -25,7 +32,7 @@ class MoviesController < ApplicationController
 
     def update
         @movie = Movie.find params[:id]
-        permitted = params[:movie].permit(:title,:rating,:release_date)
+        permitted = params[:movie].permit(:title,:description,:rating,:release_date)
         @movie.update(permitted)
         flash[:notice] = "#{@movie.title} was successfully updated."
         redirect_to movie_path(@movie)
@@ -36,5 +43,17 @@ class MoviesController < ApplicationController
         @movie.destroy
         flash[:notice] = "Movie '#{@movie.title}' deleted."
         redirect_to movies_path
+    end
+
+    def check_for_cancel
+        if (params.key?("create_cancel"))
+            flash[:notice] = "Create cancelled"
+            redirect_to movies_path
+        
+        elsif (params.key?("edit_cancel"))
+            @movie = Movie.find params[:id]
+            flash[:notice] = "Edit cancelled"
+            redirect_to movie_path(@movie)
+        end 
     end
 end
