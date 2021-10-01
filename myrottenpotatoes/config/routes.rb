eb588645c343d8bfd074b243ceec76bd3cb41fba
echo-centos <s6201012610044@email.kmutnb.ac.th>
2021-10-01 16:18:56 +0700

    Login with googld oauth

--- myrottenpotatoes/config/routes.rb
+++ myrottenpotatoes/config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :movies
   root :to => redirect('/movies')
+
+  get '/auth/:provider/callback' => 'sessions#create'
+  post 'logout' => 'sessions#destroy'
+  get 'auth/failure' => 'sessions#failure'
+  get 'auth/google_oauth2', as: 'login'
 end
--- myrottenpotatoes/db/schema.rb
+++ myrottenpotatoes/db/schema.rb
@@ -10,13 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_034815) do
+ActiveRecord::Schema.define(version: 2021_10_01_011047) do
+
+  create_table "moviegoers", force: :cascade do |t|
+    t.string "name"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.text "description"
     t.datetime "release_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
