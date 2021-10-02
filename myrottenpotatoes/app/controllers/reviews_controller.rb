c249cdf03c3f9004aafac8bb33487fafb4e53bab
echo-centos <s6201012610044@email.kmutnb.ac.th>
2021-10-02 14:58:02 +0700

    Implement review

---
+++ myrottenpotatoes/app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+    before_action :has_moviegoer_and_movie, :only => [:new, :create]
+    protected
+    def has_moviegoer_and_movie
+        unless @current_user
+        flash[:warning] = 'You must be logged in to create a review.'
+        redirect_to 'auth/google_oauth2'
+        end
+
+        unless (@movie = Movie.find_by(:id => params[:movie_id]))
+            flash[:warning] = 'Review must be for an existing movie.'
+            redirect_to movies_path
+        end
+    end
+    public
+    def new
+        @review = @movie.reviews.build
+    end
+    def create
+        @current_user.reviews << @movie.reviews.build(review_params)
+        redirect_to movie_path(@movie)
+    end
+    private
+    def review_params
+        params.require(:review).permit(:potatoes)
+    end
+end
