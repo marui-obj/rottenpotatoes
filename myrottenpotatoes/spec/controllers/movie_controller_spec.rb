cc41d76672a18fb1cec8ded846fe52bea87fbd73
tcydus <natthanan43@hotmail.com>
2021-10-04 07:48:05 +0700

    Add 3 unit test

--- myrottenpotatoes/spec/controllers/movie_controller_spec.rb
+++ myrottenpotatoes/spec/controllers/movie_controller_spec.rb
@@ -1,8 +1,44 @@
 require 'rails_helper'
 
 describe MoviesController  do
-    
-    
+    describe 'delete movie'do
+        before :each do
+        @movietest2=FactoryGirl.create(:movie)
+        end
+        it 'Delete Movie' do
+          expect{delete:destroy,params: {id: @movietest2.id}}.to change(Movie,:count).by(-1)
+        end
+        it 'Home page'do
+          delete:destroy,params:{id: @movietest2.id}
+          expect(response).to redirect_to(:action=>'index')
+        end
+
+      end
+
+      describe 'add movie' do
+        before :each do
+         @movietest={movie:{:title=>'a',:rating=>'G',:release_date=>'01-04-2000',:description=>'eiei'}}
+        end
+        it 'New Movie' do
+          expect{post:create,params:@movietest}.to change(Movie,:count).by(1)
+        end
+      end
+
+      describe 'edit movie'do 
+        before :each do
+          @movietest2=FactoryGirl.create(:movie)
+        end
+        it 'edit movie' do
+          get :edit,params:{id: @movietest2.id}
+          expect(assigns(:movie).description)==('eiei')
+        end
+        it 'detail page'do
+          post :show,params:{id: @movietest2.id}
+          expect(response).to render_template('show')
+        end
+      end
+
+
 end
 
 
--- myrottenpotatoes/spec/controllers/movie_spec.rb
+++ myrottenpotatoes/spec/controllers/movie_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
-describe TmdbController do
-  # describe 'searching TMDb' do
-  #   it 'calls the model method that performs TMDb search' 
-  #   it 'should select the Search Results template for rendering'
-  #   it 'should make the TMDb search results available to that template'
-  # end
+describe Movie, :type => :request do
+  describe 'check' do
+    fixtures :movies
+    it 'includes rating and year in full name'  do
+      movie = movies(:milk_movie)
+      expect("#{movie.title} (#{movie.rating})") == 'Milk (R)'
+      # expect("Milk (R)") == 'Milk (R)'
+    end
+    it 'can do the same thing using a factory' do 
+      # 'build' creates but doesn't save object; 'create' also saves it
+      movie = FactoryGirl.build(:movie, :title => 'Milk', :rating => 'R')
+      expect("#{movie.title} (#{movie.rating})").to eq 'Milk (R)'
+    end
+    
+  end
 end
